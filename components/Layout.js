6888e9287990c5f63d2ca24155b4829d48070356
Kaio <kaiogoes1999@gmail.com>
2022-01-31 15:08:16 -0300

    adicionando link ao header

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import Head from 'next/head';
-import { AppBar, Container, Toolbar, Typography } from '@material-ui/core';
+import NextLink from 'next/link';
+import {
+  AppBar,
+  Container,
+  Toolbar,
+  Typography,
+  Link,
+} from '@material-ui/core';
 import useStyles from '../utils/styles';
 
 export default function Layout({ children }) {
@@ -12,7 +19,20 @@ export default function Layout({ children }) {
       </Head>
       <AppBar position="static" className={classes.navbar}>
         <Toolbar>
-          <Typography>Amazona</Typography>
+          <NextLink href="/" passHref>
+            <Link>
+              <Typography className={classes.brand}>Amazona</Typography>
+            </Link>
+          </NextLink>
+          <div className={classes.grow}></div>
+          <div>
+            <NextLink href="/cart" passHref>
+              <Link>Cart</Link>
+            </NextLink>
+            <NextLink href="/login" passHref>
+              <Link>Login</Link>
+            </NextLink>
+          </div>
         </Toolbar>
       </AppBar>
       <Container className={classes.main}>{children}</Container>
